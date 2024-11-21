40141f08d4855e667c7ae5da85eb5518b0730f63
Tiago Borges <tiagoc.borges02@gmail.com>
2024-11-21 21:52:08 +0000

    changing prisma schema for vercel, creating users POST, adding vercel to package.json

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,17 +1,55 @@
+import { db } from "@/app/lib/db";
+import { hash } from "bcrypt";
 import { NextResponse } from "next/server";
 
-export const GET = () => 
+ export const GET = () => 
 {
     return new NextResponse("First API with next.");
 };
 
-/* export async function POST(req: Request) {
+ export async function POST(req: Request) {
     try {
         const body = await req.json();
         const { email, username, password } = body;
 
-        const existingUserByEmail = await
+        const existingUserByEmail = await db.user.findUnique({
+            where: { email: email }
+        });
+        if(existingUserByEmail){
+            return NextResponse.json(
+            {
+                user: null, message: "User with this email already exists"
+            },
+            {
+                status: 409
+            })
+        }
+
+        const existingUserByUsername = await db.user.findUnique({
+            where: { username: username }
+        });
+        if(existingUserByUsername){
+            return NextResponse.json(
+            {
+                user: null, message: "User with this username already exists"
+            },
+            {
+                status: 409
+            })
+        }
+
+        const hashedPassword = await hash(password, 10);
+        const newUser = await db.user.create({
+            data: {
+                email,
+                username,
+                password: hashedPassword
+            }
+        });
+        const { password: newUserPassword, ...rest } = newUser; //uma questão de segurança
+
+        return NextResponse.json({ user: rest, message:"User created"}, { status: 201});
     } catch (error) {
-        
+        return NextResponse.json({ message:"Something went wrong"}, { status: 500});
     } 
-}*/
+}
